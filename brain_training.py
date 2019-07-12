c42ba359855c474d51c3313674c586824f98fd6b
durcus <durc@inbox.ru>
2019-07-12 13:07:41 +0300

    Программа в целом готова

--- brain_training.py
+++ brain_training.py
@@ -11,26 +11,32 @@ from random import randint
 
 
 def brain_training():
-    choice = randint(1, 3)
-    number1 = randint(12, 99)
-    number2 = randint(12, 99)
-    if choice == 1:
-        result = number1 * number2
-        user_answer(choice, number1, number2, result)
-    elif choice == 2:
-        result = number1 + number2
-        user_answer(choice, number1, number2, result)
+    while True:
+        choice = randint(1, 3)
+        number1 = randint(12, 99)
+        number2 = randint(12, 99)
+        if choice == 1:
+            result = number1 * number2
+            user_answer('*', number1, number2, result)
+        elif choice == 2:
+            result = number1 + number2
+            user_answer('+', number1, number2, result)
+        else:
+            result = number1 - number2
+            user_answer('-', number1, number2, result)
+
+
+def user_answer(znak, number1, number2, result):
+    print('Сколько будет {} {} {}'.format(number1, znak, number2))
+    for i in range(3):
+        answer = input('Ваш ответ: ')
+        if answer == str(result):
+            print('Правильно!')
+            break
+        else:
+            print('Не совсем...')
     else:
-        result = number1 - number2
-        user_answer(choice, number1, number2, result)
-
-
-def user_answer(choice, number1, number2, result):
-    pass
-
-
-def compare(answer, result):
-    pass
+        print('Правильный ответ: {}'.format(result))
 
 
 brain_training()
